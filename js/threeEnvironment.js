fc4db93c038e8ca0d56873f977544f1c54a66f6c
Mathias Sletten <mathiasasletten@gmail.com>
2026-02-10 18:49:05 -0600

    mobile fov shenanigans

--- js/threeEnvironment.js
+++ js/threeEnvironment.js
@@ -84,6 +84,27 @@ function grabSphere(sphereColor) {
     }
 }
 
+export function resizeEnvironment() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const aspect = width / height;
+    const baseFOV = 75;
+    const baseAspect = 1920 / 1080;
+
+    renderer.setSize(width, height);
+    camera.aspect = aspect;
+
+    if (aspect < baseAspect) {
+        const fovRad = THREE.MathUtils.degToRad(baseFOV);
+        const hFOV = 2 * Math.atan(Math.tan(fovRad / 2) * baseAspect);
+        camera.fov = THREE.MathUtils.radToDeg(2 * Math.atan(Math.tan(hFOV / 2) / aspect));
+    } else {
+        camera.fov = baseFOV;
+    }
+
+    camera.updateProjectionMatrix();
+}
+
 function animateCamera() {
     const camTimeline = CameraWaypoints;
 
--- main.js
+++ main.js
@@ -1,32 +1,12 @@
 import * as THREE from 'three';
 import { render, incrementGlobalTimeline, getCamera, getRenderer } from "./js/threeEnvironment.js";
+import { resizeEnvironment } from './js/threeEnvironment.js';
 
 const button = document.getElementById('continueButton');
 button.addEventListener('click', incrementGlobalTimeline);
 
 // Handle Window Resize
-window.addEventListener('resize', () => {
-    const camera = getCamera();
-    const renderer = getRenderer();
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const aspect = width / height;
-    const baseFOV = 75;
-    const baseAspect = 1920 / 1080;
-
-    renderer.setSize(width, height);
-    camera.aspect = aspect;
-
-    if (aspect < baseAspect) {
-        const fovRad = THREE.MathUtils.degToRad(baseFOV);
-        const hFOV = 2 * Math.atan(Math.tan(fovRad / 2) * baseAspect);
-        camera.fov = THREE.MathUtils.radToDeg(2 * Math.atan(Math.tan(hFOV / 2) / aspect));
-    } else {
-        camera.fov = baseFOV;
-    }
-
-    camera.updateProjectionMatrix();
-});
+window.addEventListener('resize', resizeEnvironment);
 
 window.jumpTo = function(percent) {
     const totalHeight = document.body.scrollHeight - window.innerHeight;
@@ -37,5 +17,7 @@ window.jumpTo = function(percent) {
         behavior: 'smooth'
     });
 };
+
+resizeEnvironment();
 
 render();
